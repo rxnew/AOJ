6fd01ccbe93229f3f57cddf9e95cd1122bfe757a
rxnew <rxnew.axdseuan@gmail.com>
2016-06-17 13:46:31 +0900

    DPL_2_A

---
+++ DPL_2_A/DPL_2_A_commented.cpp
@@ -0,0 +1,35 @@
+// TSP (Traveling Salesman Problem)
+// bitDP
+
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; i++)
+using namespace std;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+constexpr auto INF = numeric_limits<int>::max();
+
+inline auto add(int lhs, int rhs) -> int {
+  return lhs == INF || rhs == INF ? INF : lhs + rhs;
+}
+
+// S := (すでに辿った頂点番号のビットを立てた値)
+// dp[S][v] := (状態 S で頂点 v にいるとき残りの頂点を辿る最短距離)
+// dp[S][v] = min(dp[S][v], dp[S’][v’] + d[v][v’])
+auto tsp(int V, const vvi& d) -> int {
+  vvi dp(1 << V, vi(V, INF));
+  dp.back()[0] = 0;
+  for(int S = (1 << V) - 1; S >= 0; S--) REP(v, V) REP(u, V)
+    if(!(S & 1 << u)) dp[S][v] = min(dp[S][v], add(dp[S | 1 << u][u], d[v][u]));
+  return dp[0][0];
+}
+
+auto main() -> int {
+  int V, E, si, ti, di;
+  cin >> V >> E;
+  vvi d(V, vi(V, INF));
+  while(cin >> si >> ti >> di) d[si][ti] = di;
+  cout << [](int x){return x == INF ? -1 : x;}(tsp(V, d)) << endl;
+  return 0;
+}
